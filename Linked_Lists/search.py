420f131c948e3231f56e650627b81e768ece0f60
Hushcoder <chauhanvi360@gmail.com>
2025-02-05 14:57:03 +0530

    LL

---
+++ Linked_Lists/search.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+    
+    def search(head,x):
+        index  = 1
+        curr = head
+        while curr != None:
+            if curr.key == x:
+                return index
+            index += 1
+            curr = curr.next
+        return -1
+
+if __name__ == '__main__':
+    node_1 = Node(10)
+    node_2 = Node(11)
+    node_3 = Node(30)
+    node_4 = Node(15)
+    node_1.next = node_2
+    node_2.next = node_3
+    node_3.next = node_4
+    head = node_1
+
+    ind = Node.search(head,15)
+    print(f'Index of given key is {ind}')
